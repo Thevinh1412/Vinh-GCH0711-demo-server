32ec780558fe80d6de385c8182ed6a7a49d614e5
Thevinh1412 <vinhdtgch17230@fpt.edu.vn>
2020-05-20 09:25:13 +0700

    Update fixbug.php

--- fixbug.php
+++ fixbug.php
@@ -1,1 +1,14 @@
-<htmlentities
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1> My first PHP pagee</h1>
+
+<?php
+echo "Wednesday !!!@@@";
+echo "cloud computing";
+echo "change in fixbug branch";
+?>
+
+</body>
+<html>
