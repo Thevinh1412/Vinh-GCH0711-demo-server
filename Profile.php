bd32f548e80281974ecd6cfd012e02d6af8fe191
Thevinh1412 <vinhdtgch17230@fpt.edu.vn>
2020-05-20 10:43:09 +0700

    Update Profile.php

--- Profile.php
+++ Profile.php
@@ -5,7 +5,15 @@
 <h1> Profile</h1>
 
 <?php
-<h1> Name: DO THE VINH</h1>
+<form action="action_page.php">
+First name:<br>
+<input type="text" name="firstname">
+<br>
+Last name:<br>
+<input type="text" name="lastname">
+<br><br>
+<input type="submit">
+</form>
 <img src="cat-colors-daylight-674580.jpg" alt="My picture">
 <a href="https://www.facebook.com/chikkippo">My facebook</a>
 ?>
