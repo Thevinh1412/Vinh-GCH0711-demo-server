882f8ac566d52aa3194ba1fb0d296fa08113785e
Thevinh1412 <vinhdtgch17230@fpt.edu.vn>
2020-05-20 10:34:40 +0700

    update

---
+++ Profile.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1> Profile</h1>
+
+<?php
+<h1> Name: DO THE VINH</h1>
+<img src="cat-colors-daylight-674580.jpg" alt="My picture">
+<a href="https://www.facebook.com/chikkippo">My facebook</a>
+?>
+
+</body>
+<html>
